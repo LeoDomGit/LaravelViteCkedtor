29718c4737fc074d9a91ff0893ef63f0d9456d85
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-06-29 14:37:59 +0700

    Commit

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web']], function () {
-    \UniSharp\LaravelFilemanager\Lfm::routes();
-});
