8ceb7c0f480dcf1adf406a1da3926e55b81149ab
Dom <leodomsolar@gmail.com>
2024-07-14 08:25:38 +0700

    Commit

--- plugins/services/routes/web.php
+++ plugins/services/routes/web.php
@@ -7,3 +7,8 @@ Route::middleware(['web', CheckLogin::class])->group(function () {
     Route::resource('services', ServicesController::class);
     Route::post('/update-services/{id}',[ServicesController::class,'update']);
 });
+
+Route::prefix('api/services/')->group(function () {
+    Route::get('home', [ServicesController::class,'api_home']);
+
+});
