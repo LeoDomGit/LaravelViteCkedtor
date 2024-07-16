e189c5e4cea9a03d2b429d40d8097f9927d8d951
Khanh Duy Dev <khanhduytran1803@gmail.com>
2024-07-16 16:02:46 +0700

    new commit

--- plugins/bookings/routes/web.php
+++ plugins/bookings/routes/web.php
@@ -3,12 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use Leo\Bookings\Controllers\BookingController;
 
-Route::apiResource('api/bookings', BookingController::class);
 Route::apiResource('bookings', BookingController::class);
 
 Route::prefix('api')->group(function () {
-    Route::prefix('bookings')->group(function () {
-        Route::get('/',[BookingController::class,'api_home']);
-    });
-
+    Route::post('/bookings',[BookingController::class,'store']);
+    Route::get('/customers',[BookingController::class,'getCustomer']);
+    Route::get('/customers/{id}',[BookingController::class,'getBillsCustomer']);
+    Route::get('/checkOut/{id}',[BookingController::class,'createBill']);
+    Route::put('/bookings/{id}',[BookingController::class,'update']);
+    Route::get('/bookings',[BookingController::class,'api_home'])->middleware('auth:sanctum');
+    Route::get('/bookings/nhan-vien',[BookingController::class,'api_nhan_vien'])->middleware('auth:sanctum');
+    Route::get('/submitBooking/{id}',[BookingController::class,'api_submitbooking_nhan_vien'])->middleware('auth:sanctum');
+    Route::get('/cancelBooking/{id}',[BookingController::class,'api_cancelbooking_nhan_vien'])->middleware('auth:sanctum');
 });
---
+++ plugins/services/src/Models/ServiceBillsDetails.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Leo\Services\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceBillsDetails extends Model
+{
+    use HasFactory;
+    protected $table = 'service_bill_details';
+    protected $fillable = ['id_bill', 'id_service', 'id_booking', 'created_at', 'updated_at'];
+
+    public function service_bills()
+    {
+        return $this->belongsTo(ServiceBills::class, 'id_bill');
+    }
+}
