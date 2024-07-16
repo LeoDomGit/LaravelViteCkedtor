75717b0177c5bee7ae7e807eba0312e32c509b20
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-16 09:51:54 +0700

    Commit

--- packages/bookings/routes/web.php
+++ packages/bookings/routes/web.php
@@ -6,6 +6,7 @@ use Leo\Bookings\Controllers\BookingController;
 Route::apiResource('bookings', BookingController::class);
 
 Route::prefix('api')->group(function () {
+    Route::post('/bookings',[BookingController::class,'store']);
     Route::get('/bookings',[BookingController::class,'api_home'])->middleware('auth:sanctum');
     Route::get('/bookings/nhan-vien',[BookingController::class,'api_nhan_vien'])->middleware('auth:sanctum');
     Route::get('/submitBooking/{id}',[BookingController::class,'api_submitbooking_nhan_vien'])->middleware('auth:sanctum');
