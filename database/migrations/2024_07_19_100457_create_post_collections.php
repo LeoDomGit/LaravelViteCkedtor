297bf57bdfb78cabdbb9c5df185daf1d6697c6fd
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-19 10:08:30 +0700

    Update 2024_07_19_100457_create_post_collections.php

--- database/migrations/2024_07_19_100457_create_post_collections.php
+++ database/migrations/2024_07_19_100457_create_post_collections.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name',255);
             $table->string('slug',255);
+            $table->boolean('status')->default(0);
             $table->timestamps();
         });
     }
