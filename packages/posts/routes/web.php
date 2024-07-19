353136397092190ea468ac1c56876f4507f78b3f
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-19 10:59:52 +0700

    Commit

--- packages/posts/routes/web.php
+++ packages/posts/routes/web.php
@@ -1,2 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Leo\Post\Controllers\PostCateController;
+use App\Http\Middleware\CheckLogin;
+Route::middleware(['web', CheckLogin::class])->group(function () {
+    Route::resource('post-collections', PostCateController::class);
+});
+
+Route::prefix('api')->group(function () {
+    Route::get('/post-collections',[PostCateController::class,'api_index']);
+    Route::get('/post-collections/{id}',[PostCateController::class,'api_show']);
+});
